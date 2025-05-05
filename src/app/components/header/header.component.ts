2703b3eaa1f452aa47e11909cb2f94f4a1edb4be
Oravlah <oravla.hhh@gmail.com>
2025-05-04 22:20:18 -0400

    se realizaron cambios dentro del componente header, se añade la funcionalidad para el boton en vista movil

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export default class HeaderComponent {
+  isMobileMenuOpen = false;
 
   constructor() {}
+
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 
 
 }
